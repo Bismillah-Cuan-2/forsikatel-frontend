233b435d7d855c14bbe32555c7e7b0c92009a813
rifqihananto <127004043+Killerz1724@users.noreply.github.com>
2025-02-10 15:16:25 +0700

    Add header logo component ver2

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -7,8 +7,8 @@ import logoTelkom from "../assets/images/logo_telkom.png";
 const HeaderLogo = () => {
   return (
     <header className="flex gap-5 w-[29rem]">
-        <img className="object-contain" src={logoAkhlak} alt="logo akhlak" />
         <img className="object-contain" src={logoBumn} alt="logo bumn" />
+        <img className="object-contain" src={logoAkhlak} alt="logo akhlak" /> 
         <img className="object-contain" src={logoForsikatel} alt="logo forsikatel" />
         <img className="object-contain" src={logoTelkom} alt="logo telkom" />
         <img className="object-contain" src={logoTelkomAkses} alt="logo telkom akses" />
