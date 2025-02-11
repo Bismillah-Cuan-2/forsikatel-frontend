3905d6f46e2a1adaa23548165ae9a8611c8b5f9a
Thoriq Afif <mthoriqafif01@gmail.com>
2025-02-11 14:10:38 +0700

    update sidebar design

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,14 @@
 import Button from "./Button"
-import berandaIcon from "../assets/vectors/beranda-icon.svg"
-import haditsDankultumIcon from "../assets/vectors/hadits-kultum-icon.svg"
-import progressHarianIcon from "../assets/vectors/progress-icon.svg"
-import rekapIcon from "../assets/vectors/rekap-icon.svg"
-import setoranIcon from "../assets/vectors/setor-icon.svg"
-import logoutIcon from "../assets/vectors/logout-icon.svg"
+import berandaIcon from "../assets/svg/beranda-icon.svg"
+import haditsDankultumIcon from "../assets/svg/hadits-kultum-icon.svg"
+import progressHarianIcon from "../assets/svg/progress-icon.svg"
+import rekapIcon from "../assets/svg/rekap-icon.svg"
+import setoranIcon from "../assets/svg/setor-icon.svg"
+import logoutIcon from "../assets/svg/logout-icon.svg"
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex min-w-64 flex-col justify-center items-center">
       <div className="flex flex-col justify-around h-full">
         <div className="flex flex-col gap-4">
           <Button Icon={berandaIcon} >Beranda</Button>
@@ -17,8 +17,8 @@ const SideBar = () => {
           <Button Icon={rekapIcon}>Rekapitulasi Mengaji</Button>
           <Button Icon={setoranIcon}>Setoran Mengaji</Button>
         </div>
-        <div className="flex flex-col justify-center">
-          <Button Icon={logoutIcon} color="bg-neutral-100" className="justify-center">Logout</Button>
+        <div className="flex flex-col justify-center px-8">
+          <Button Icon={logoutIcon} color="bg-neutral-100" className="justify-center text-primary-300">Keluar Akun</Button>
         </div>
       </div>
     </div>
