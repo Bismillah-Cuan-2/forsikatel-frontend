af22669b1f0bf8fc3f795c60d8e5880cd867a377
Thoriq Afif <mthoriqafif01@gmail.com>
2025-02-14 20:32:05 +0700

    design setor ngaji page for mobile

---
+++ src/components/SetorNgajiHeader.tsx
@@ -0,0 +1,16 @@
+
+
+const SetorNgajiHeader = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-primary-300">
+            Setoran Ngaji Hari Ini
+        </h1>
+        <p className="font-source font-semibold text-neutral-900">
+            Yuk catat bacaanmu dan lihat progresnya! Berapa Juz yang Kamu Baca Hari Ini?
+        </p>
+    </div>
+  )
+}
+
+export default SetorNgajiHeader
